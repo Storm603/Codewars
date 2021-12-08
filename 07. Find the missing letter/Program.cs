0d43873140c40804934f8e6849bca4a59666c3ec
Storm603 <93555141+Storm603@users.noreply.github.com>
2021-12-08 22:57:21 +0200

    Add files via upload

---
+++ 07. Find the missing letter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Find_the_missing_letter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] array = Console.ReadLine().ToCharArray();
+            Console.WriteLine(FindMissingLetter(array));
+        }
+
+        public static char FindMissingLetter(char[] array)
+        {
+            int zero = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                char currcharNum = array[i];
+                if (zero + i != currcharNum)
+                {
+                    currcharNum--;
+                    return char.Parse(currcharNum.ToString());
+                }
+            }
+
+            return ' ';
+        }
+    }
+}
---
+++ T08. Split Strings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace T08._Split_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            Console.WriteLine(string.Join(", ", Solution(str)));
+        }
+
+        public static string[] Solution(string str)
+        {
+            int groups = str.Length / 2;
+            if (str.Length % 2 == 1) groups++;
+
+            string[] arr = new string[groups];
+
+            int count = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i > 0 && i % 2 == 1)
+                {
+                    arr[count] = $"{str[i - 1]}{str[i]}";
+                    count++;
+                }
+
+                else if (i == str.Length -1)
+                {
+                    arr[count] = $"{str[i]}_";
+                }
+            }
+
+            return arr;
+        }
+    }
+}
